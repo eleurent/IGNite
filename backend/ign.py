3d94ab36c16ffd9c6382ef8af7aa91768ffedc89
Edouard Leurent <eleurent@gmail.com>
2024-05-13 13:19:31 +0100

    Update ows:Identifier for ign backend

--- backend/ign.py
+++ backend/ign.py
@@ -82,7 +82,7 @@ class IGNMap(TiledMap):
 
         # Layer selection
         layer_elements = [elt for elt in root.findall('default:Contents/default:Layer', dict_ns) if
-                          elt.findall('ows:Identifier', dict_ns)[0].text == 'GEOGRAPHICALGRIDSYSTEMS.MAPS.OVERVIEW']
+                          elt.findall('ows:Identifier', dict_ns)[0].text == 'GEOGRAPHICALGRIDSYSTEMS.PLANIGNV2']
         layer_elements = layer_elements[0].findall('default:TileMatrixSetLink', dict_ns)[
             0].getchildren()  # Returns a list elts[TileMatrixSet, TileMatrixSetLimits]
 
